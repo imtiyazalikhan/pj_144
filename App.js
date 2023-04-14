fb7c5d6a7d480a842ad0e33c6a07476812ef0406
imtiyazalikhan <65025085+imtiyazalikhan@users.noreply.github.com>
2023-04-14 14:10:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import HomeScreen from "./screens/Home";
+import RecommendedarticleScreen from "./screens/Recommendation";
+import PopulararticleScreen from "./screens/Popular";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default function App() {
+  return <AppContainer />;
+}
+
+const AppTopNavigation = createMaterialTopTabNavigator({
+  Recommendedarticle: {
+    screen: RecommendedarticleScreen,
+    navigationOptions: {
+      tabBarLabel: "Recommended",
+      tabBarOptions: {
+        tabStyle: { backgroundColor: "#fff" },
+        labelStyle: { color: "#000" },
+        indicatorStyle: { backgroundColor: "#000" }
+      }
+    }
+  },
+  Populararticle: {
+    screen: PopulararticleScreen,
+    navigationOptions: {
+      tabBarLabel: "Popular",
+      tabBarOptions: {
+        tabStyle: { backgroundColor: "#fff" },
+        labelStyle: { color: "#000" },
+        indicatorStyle: { backgroundColor: "#000" }
+      }
+    }
+  }
+});
+
+const AppStackNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        headerShown: false
+      }
+    },
+    AppTopNav: {
+      screen: AppTopNavigation,
+      navigationOptions: {
+        headerBackTitle: null,
+        headerTintColor: "#fff",
+        headerTitle: "Recommended article",
+        headerStyle: {
+          backgroundColor: "#d500f9"
+        },
+        headerTitleStyle: {
+          color: "#fff",
+          fontWeight: "bold",
+          fontSize: RFValue(18)
+        }
+      }
+    }
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppStackNavigator);
